7dd39cd4ff29bdae468af4e2c2580917edfa1c64
banmitsu@gmail.com <banmitsu@gmail.com>
2015-09-21 17:31:12 +0800

    readlmdb.py added

--- scripts/readlmdb.py
+++ scripts/readlmdb.py
@@ -10,10 +10,14 @@ import matplotlib.pyplot as plt
 LMDB_PATH = sys.argv[1]
 env = lmdb.open(LMDB_PATH, readonly=True, lock=False)
 
+OUTPUT_PATH = sys.argv[2]
+
 datum = caffe_pb2.Datum()
 
 num_datum = 60000
 visualize = False
+
+X = []
 
 with env.begin() as txn:
 	cur = txn.cursor()
@@ -22,12 +26,18 @@ with env.begin() as txn:
 			#cur.first()
 			break
 		key, value = cur.item()
-		datum.ParseFromString(value)
-		data = np.array(bytearray(datum.data))
+		datum.ParseFromString(value)	
 		# img_data = np.array(bytearray(datum.data)).reshape(datum.channels, datum.height, datum.width)
-		if visualize:
-			plt.imshow(img_data.transpose([1,2,0]))
-			plt.show()
+		if datum.width == 1 & datum.height == 1 & datum.channels == 1:
+			data = np.array(datum.float_data)
+		else:
+			data = np.array(datum.float_data).reshape(datum.channels, datum.height, datum.width)
+		#if visualize:
+		#	plt.imshow(img_data.transpose([1,2,0]))
+		#	plt.show()
+		X.append(data)
+		print key, data
+		# break
 
-		# print img_data
-		print key
+np.savetxt(OUTPUT_PATH, X, newline='\n', fmt="%1.6f")
+
