3369ee57947cbc1745bf3dcf40171c2b5d81ff2b
banmitsu <banmitsu@gmail.com>
2015-12-16 16:19:38 +0800

    scripts.m updated

--- scripts.m
+++ scripts.m
@@ -1,25 +1,26 @@
 %I = imread('/Users/felisjhou/Downloads/tid2013/reference_images/I01.BMP');
 %I_p = imread('/Users/felisjhou/Downloads/tid2013/distorted_images/I01_01_1.BMP');
 
+
 fileID = fopen('smc_lst.txt','r');
-C = textscan(fileID, '%s%s%s');
+C = textscan(fileID, '%f%s%s%s');
 fclose(fileID);
 
 n = cellfun('length',C);
 
 for i=1:1:n(1)
-    I  = imread(C{1,1}{i,1});
-    I_p= imread(C{1,2}{i,1});
+    I  = imread(C{1,2}{i,1});
+    I_p= imread(C{1,3}{i,1});
     I  = rgb2gray(I);
     I_p= rgb2gray(I_p);
     
     [feature, err_flag] = feature_smc1(I, I_p);
     if(err_flag)
-        disp(C{1,1}{i,1})
         disp(C{1,2}{i,1})
         disp(C{1,3}{i,1})
+        disp(C{1,4}{i,1})
     else
-        fileID = fopen(C{1,3}{i,1}, 'w');
+        fileID = fopen(C{1,4}{i,1}, 'w');
         fprintf(fileID,'%f\n', feature);
         fclose(fileID);
     end
